5e35a1873934a25bf4884001130398f681f5486d
Grad <andy.sommerlot@gmail.com>
2017-01-23 20:29:40 -0500

    rename

--- auto_train.R
+++ auto_train.R
@@ -1,22 +1,21 @@
 ### function to run all three ...
-auto.train <- function(train,
+#========================================================
+start.train <- function(train,
                     y_name,
                     y_type,
                     wd = getwd()) {
   setwd(wd)
-  autotrain.rf(train = train,
+  start.rf(train = train,
                y_name = y_name,
                y_type = y_type)
 
-  autotrain.gbm(train = train,
+  start.gbm(train = train,
                y_name = y_name,
                y_type = y_type)
 
-  autotrain.deeplearning(train = train,
+  start.deeplearning(train = train,
                 y_name = y_name,
                 y_type = y_type)
-
-
 }
 
 
