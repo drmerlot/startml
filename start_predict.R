3d70ca1349a5321f11b4a09d2ee10313f2af7ad8
Grad <andy.sommerlot@gmail.com>
2017-02-01 18:00:12 -0500

    predict on list of best

--- start_predict.R
+++ start_predict.R
@@ -1,4 +1,12 @@
 #============= predict with the random models.
 
 
-start.predict <- function(test, models)
+start.predict <- function(test, best_models) {
+  predctions_glm <- lapply(best_models[[1]], h2o.predict, newdata = test)
+  predctions_deeplearning <- lapply(best_models[[2]], h2o.predict, newdata = test)
+  predctions_rf <- lapply(best_models[[3]], h2o.predict, newdata = test)
+  predictions <- list(predctions_glm, predctions_deeplearning, predctions_rf)
+}
+
+te <- start.predict(test, best_models)
+te <- h2o.predict(best_models, test)
