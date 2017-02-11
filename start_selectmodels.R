665c34c9aeff242e0ed742f67edfe1d78dbfc27a
Grad <andy.sommerlot@gmail.com>
2017-02-11 14:34:25 -0500

    add correlation calculation

--- start_selectmodels.R
+++ start_selectmodels.R
@@ -1,22 +1,32 @@
 #===========================================================
 # get best models by performance and correlation threshold for ensemble or something
-
-eval_metric = AUC
+eval_metric = "RMSE"
 eval_threshold = 0.5 # performance threshold
 correlation_threshold = 0.7 # pearson correlation
 
-start.selectmodels <- function(model_list, test eval_threhold, correlation_threhold) {
+start.selectmodels <- function(model_list,
+                               test,
+                               eval_metric,
+                               eval_threshold = 0,
+                               correlation_threhold = 1) {
   if(eval_metric == "AUC") {
-    fun <- function(x, y) {
-      x >= y
+    eval_fun <- function(a, b) {
+      a >= b
     }
   } else {
-    fun <- function(x, y) {
-      x <= y
+    eval_fun <- function(a, b) {
+      a <= b
       }
   }
 
+  prediction_list <- start.predict(test, model_list)
+  predictions <- h2o.cbind(predictions)
+  correlations <- h2o.cor(predictions)
 
-  top_models <- model_list[sorted_models$model_list_num[1:number_top_models]]
-  top_models
+  metrics <- unlist(start.validmetric(model_list, eval_metric = eval_metric))
+
+  selected_models
 }
+
+
+
