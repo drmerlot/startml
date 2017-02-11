0ad79e7ea085d654a197876e414c81882e346787
Grad <andy.sommerlot@gmail.com>
2017-02-11 14:34:57 -0500

    remove print out

--- start_predict.R
+++ start_predict.R
@@ -1,7 +1,7 @@
 #==========================================================
 #predict with selected models from start_autotrain
 start.predict <- function(test, selected_models) {
-  cat("Predicting on New Data With Selected Models\n")
+  #cat("Predicting on New Data With Selected Models\n")
   predictions <- lapply(selected_models, h2o.predict, newdata = test)
   predictions
 }
