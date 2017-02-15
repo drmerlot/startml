97cd877d3bc6d3230392348d10997518cd904f04
Grad <andy.sommerlot@gmail.com>
2017-02-14 23:25:59 -0500

    skeleton of plot function

---
+++ start.plot.R
@@ -0,0 +1,14 @@
+# Plot binomial models ==========================
+# right now, uses model list,
+# change this to ml object when object is defined
+# this adds ggplot2 dependency making the future package
+# need gpl3 license.
+
+start.plot <- function(model_list, valid, test) {
+
+  if(!is.null(valid)) {
+  }
+
+  if(class(model_list[[1]] == "H2OBinomialModel")) {
+  }
+}
