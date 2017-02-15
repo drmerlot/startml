97f11cf72a2a1c48534b1f6591a68b1676657d7d
Grad <andy.sommerlot@gmail.com>
2017-02-15 14:07:47 -0500

    working s4 class mlstack

--- start_mlclass.R
+++ start_mlclass.R
@@ -1,5 +1,5 @@
 # trying to set a class
-setClass("ml", slots = c(models = "list",
+setClass("mlstack", slots = c(models = "list",
                          train = "list",
                          valid = "list",
                          test = "list",
@@ -8,6 +8,22 @@ setClass("ml", slots = c(models = "list",
                          predict_valid = "list",
                          predict_test = "list",
                          predict_newdata = "list",
-                         index = "data.frame"
-                         ))
+                         index = "data.frame"))
+
+
+# doesn't work  ==============================
+# setGeneric("fill_models", function (x) standardGeneric("fill_models"))
+# setMethod("fill_models", c(x="ml"), function (x) x@models)
+#
+#
+# setGeneric("fill_models<-", function (x, ...) standardGeneric("fill_models<-"))
+# setMethod("fill_models<-", c(x="ml"), function (x, value) {
+#   x@models <- value
+#   #if (validObject(x)) return(x)
+# })
+
+# fill_models(out) <- model_list
+
+# end doesn't work ==============================
+
 
