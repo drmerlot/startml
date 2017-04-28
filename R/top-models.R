158c05d96af33621b98922d51339dc568db6d751
System Administrator <andy.sommerlot@gmail.com>
2017-04-28 17:50:47 -0400

    rename variables in top models function

--- R/top-models.R
+++ R/top-models.R
@@ -1,6 +1,6 @@
 #===========================================================
 # get best models by performance threshold
 top_models <- function(sorted_models, model_list, number_top_models = 1) {
-  top_models <- model_list[sorted_models$model_list_num[1:number_top_models]]
-  top_models
+  all_top_models <- model_list[sorted_models$model_list_num[1:number_top_models]]
+  all_top_models
 }
