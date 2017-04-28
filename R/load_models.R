7979c1468a7d3ed08d9fc62c0b20e0872a40b01c
System Administrator <andy.sommerlot@gmail.com>
2017-04-28 16:43:09 -0400

    change naming scheme

--- R/load_models.R
+++ R/load_models.R
@@ -1,6 +1,6 @@
 #=======================
 # load saved h2o models
-start.loadmodels <- function(path) {
+load_models <- function(path) {
   all_model_files <- list.files(path, full.names = TRUE)
   all_models  <- sapply(all_model_files, function(m) h2o.loadModel(m))
   all_models
