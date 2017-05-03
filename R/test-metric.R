a7446246dc7124a3f243beafdd68803df9593e7f
andrewsommerlot <andy.sommerlot@gmail.com>
2017-05-03 14:17:13 -0400

    remove need to evaluate test metrics in R ram space

--- R/test-metric.R
+++ R/test-metric.R
@@ -1,19 +1,18 @@
 #===============================================
 # get test holdout metrics from models # depends on Metrics package for now
-test_metric <- function(prediction_list, test, eval_metric, y) {
-  predictions_dataframe <- lapply(prediction_list, as.data.frame)
+test_metric_h2o <- function(prediction_list, test, eval_metric, y) {
   if(eval_metric == "AUC" | eval_metric == "logloss") {
-    predictions <- lapply(predictions_dataframe, function(x)x[,3])
+    predictions <- lapply(prediction_list, function(x)x[,3])
   } else {
-    predictions <- lapply(predictions_dataframe, function(x)x[,1])
+    predictions <- lapply(prediction_list, function(x)x[,1])
   }
-  actual <- as.data.frame(test[,y])[,1]
+  actual <- test[,y]
   if(eval_metric == "logloss") {
     metric <- lapply(predictions, FUN = logLoss, actual = actual)
   } else if(eval_metric == "MSE") {
     metric <- lapply(predictions, FUN = mse, actual = actual)
   } else if(eval_metric == "RMSE") {
-    metric <- lapply(predictions, FUN = mse, actual = actual)
+    metric <- lapply(predictions, FUN = rmse, actual = actual)
   } else if(eval_metric == "MAE") {
     metric <- lapply(predictions, FUN = mae, actual = actual)
   } else if(eval_metric == "AUC") {
@@ -23,7 +22,7 @@ test_metric <- function(prediction_list, test, eval_metric, y) {
   }else if(eval_metric == "RMSLE") {
     metric <- lapply(predictions, FUN = rmsle, actual = actual)
   } else {
-    stop("Choose an eval metric: logloss, MSE, RMSE, MAE, AUC, mean_per_class_error = 'ce'")
+    stop("Choose an eval metric: logloss, MSE, RMSE, MAE, AUC, mean_per_class_error")
   }
   metric
 }
