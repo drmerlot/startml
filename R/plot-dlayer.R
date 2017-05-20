4edf83e923c75274676ee85c2ec5d51724510485
andrewsommerlot <andy.sommerlot@gmail.com>
2017-05-20 13:00:03 -0400

    add 3d plot

--- R/plot-dlayer.R
+++ R/plot-dlayer.R
@@ -14,15 +14,15 @@ plot_dlayer <- function(model,
   dat <- vis_data[samp,]
   cat("Getting hidden layer values from DL model\n")
   dlayer <- as.data.frame(h2o.deepfeatures(model, dat, layer = layer))
-  cat("Starting tSNE dimentionality reduction")
-  dat_tsne <- tsne(X = dlayer, 
-                   k = dimentions, 
-                   initial_dims = dim(dlayer)[2], 
-                   max_iter = tsne_iter)
-  vis_label <- as.data.frame(view_label)[,1]
-  dat_plot <- data.frame(dat_tsne, vis_label)
   if(dimentions == 2) {
-    names(dat_plot) <- c("dl.hl.1", "dl.hl.2")
+    cat("Starting tSNE dimentionality reduction")
+    dat_tsne <- tsne(X = dlayer, 
+                     k = dimentions, 
+                     initial_dims = dim(dlayer)[2], 
+                     max_iter = tsne_iter)
+    vis_label <- as.data.frame(view_label)[,1]
+    dat_plot <- data.frame(dat_tsne, vis_label)
+    names(dat_plot) <- c("dl.hl.1", "dl.hl.2", "vis_label")
     p <- ggplot(dat_plot) + 
       geom_point(aes(x = dl.hl.1, y = dl.hl.2, color = vis_label),
                  alpha = 0.7) +
@@ -32,7 +32,20 @@ plot_dlayer <- function(model,
       guides(color = guide_legend(title = label)) +
       theme_classic(base_size =  12)
   } else if(dimentions == 3) {
-    stop("3D plot in the works. Requires Plotly")
+    cat("Starting tSNE dimentionality reduction")
+    dat_tsne <- tsne(X = dlayer, 
+                     k = dimentions, 
+                     initial_dims = dim(dlayer)[2], 
+                     max_iter = tsne_iter)
+    vis_label <- as.data.frame(view_label)[,1]
+    
+    dat_plot <- data.frame(dat_tsne, vis_label)
+    names(dat_plot) <- c("dl.hl.1", "dl.hl.2", "dl.hl.3", "vis_label")
+    p <- plot_ly(dat_plot, x = ~dl.hl.1, y = ~dl.hl.2, z = ~dl.hl.3, color = ~vis_label) %>%
+      add_markers() %>%
+      layout(scene = list(xaxis = list(title = "tSNE Dim 1"),
+                          yaxis = list(title = "tSNE Dim 2"),
+                          zaxis = list(title = "tSNE Dim 3")))
   } else { 
     stop("Dimentions must be set to 2 or 3")  
   }
