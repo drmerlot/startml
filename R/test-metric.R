c713f98a163452588d3b4b47614bfa90294624b4
andrewsommerlot <andy.sommerlot@gmail.com>
2017-06-20 10:53:43 -0400

    change error message

--- R/test-metric.R
+++ R/test-metric.R
@@ -2,9 +2,7 @@
 #'
 #' Caculate performance metrics from models on new data. Depends on Metrics package.
 #' @param prediction_list List object of H2O frames containing predictions.
-#' No Default.
 #' @param test H2O frame object containing labeled data for model evaluation.
-#' No Default.
 #' @param eval_metric Character object one of logloss, MSE, RMSE, MAE, AUC, or mean_per_class_error.
 #' @param y Character object of length 1 identifying the column name of the target variable. No Default.
 #' @return List object same length as prediction_list containing performance of each model on test input with selected metric.
@@ -16,7 +14,7 @@ test_metric <- function(prediction_list, test, eval_metric, y) {
   } else {
     predictions <- lapply(prediction_list, function(x)x[,1])
   }
-  actual <- test[,y]
+  actual <- test[, y]
   if(eval_metric == "logloss") {
     metric <- lapply(predictions, FUN = logLoss, actual = actual)
   } else if(eval_metric == "MSE") {
