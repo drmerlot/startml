3775536d56dca587296ac86b2f11656207aafc97
andrewsommerlot <andy.sommerlot@gmail.com>
2017-06-20 10:52:03 -0400

    update startml to run with or without threshold selection

--- R/startml.R
+++ R/startml.R
@@ -23,6 +23,7 @@
 #' @param runtime_secs Character Object which sets the length of time each grid search will run. Defaults to 20, thus the default runtime is 20 sec * (length of algorimths) = 1 minute.
 #' @param split_seed Random seed for splitting labeled data into train, validation, and test components. Currently, startml only supports random sampling splits, this argument sets the random seed for these splits, making the data set separation process reproducible. Since this is a "naive" random split, labeled data should be shuffled before hand.
 #' @param wd Character object defining file path where resulting modeling will be saved. Defualts to current working directory.
+#' @param trim Boolean. When TRUE, output is trimmed with eval_threshold, correlation_threshold, or number_top_models. When FALSE, all models are returned. Default FALSE.
 #' @param number_top_models Numeric object indicating number of top models to return. Defualt is 10. If number entered is greater than number of model, whole model list is returned.
 #' @param eval_threshold Numeric object defining the performance threshold models must meet to be used in prediction. Is minimum for maximization loss function (i.e., AUC) and maximum for minimization loss functions (logloss, MSE, etc). Default is NULL, returns models without performance consideration.
 #' @param correlation_threshold Numeric object defining the maximum person correlation allowed in the group of resulting models. If two models show high correlation, the one with surperior performance will be kept and the other dropped. Value ranges from -1 to 1, default is NULL, returning models without correlation considered.
@@ -56,6 +57,7 @@ startml <-  function(labeled_data,
                       percent_test_holdout = 10,
                       runtime_secs = 10,
                       split_seed = NULL,
+                      trim = FALSE,
                       number_top_models = NULL,
                       eval_threshold = NULL,
                       correlation_threshold = 0,
@@ -131,59 +133,61 @@ startml <-  function(labeled_data,
                                 runtime_secs = runtime_secs,
                                 wd = wd)
   # ===================================================================
-
-  if(!is.null(number_top_models)) {
-    cat("\nChoosing Top Performing Models on Validation")
-    sorted_models <- sort_models(all_models,
+  if(trim == TRUE) {
+    if(!is.null(number_top_models)) {
+      cat("\nChoosing Top Performing Models on Validation")
+      sorted_models <- sort_models(all_models,
                                  eval_metric = eval_metric)
-    selected_models <- top_models(sorted_models,
+      selected_models <- top_models(sorted_models,
                                   all_models,
                                   number_top_models = number_top_models)
-  } else {
-    cat("\nChoosing Models on Test based on Performance and Correlation Thresholds\n")
-    selected_models <- select_models(model_list = all_models,
-                                     test = test,
-                                     eval_metric = eval_metric,
-                                     eval_threshold = eval_threshold,
-                                     y = y,
-                                     correlation_threshold = correlation_threshold)
-  }
-    cat("\nSaving Train Predictions with Selected Models\n")
-    train_predictions <- predict_blob(test = train, selected_models)
-    cat("\nSaving Valid Predictions with Selected Models\n")
-    valid_predictions <- predict_blob(test = valid, selected_models)
-    cat("\nSaving Test Predictions with Selected Models\n")
-    test_predictions <- predict_blob(test = test, selected_models)
-    cat("\nPredicting on New Data with Selected Models\n")
-    newdata_predictions <- predict_blob(test = new_data, selected_models)
-
-    # needs work.
-    # make the index dataframe, trivially all 1s for shared holout
-    index = data.frame(model_num = seq(1, length(selected_models)),
-                       train_id = rep(1, length(selected_models)),
-                       valid_id = rep(1, length(selected_models)),
-                       test_id = rep(1, length(selected_models)))
-
-    # =================================================
-    if(return_dataframe == FALSE) {
-      # build the output object of new class mlblob
-      mlout <- new("mlblob",
-                   models = selected_models,
-                   labeled_data = list(labeled_data),
-                   train = list(train),
-                   valid = list(valid),
-                   test = list(test),
-                   new_data = list(new_data),
-                   predict_train = train_predictions,
-                   predict_valid = valid_predictions,
-                   predict_test = test_predictions,
-                   predict_newdata = newdata_predictions,
-                   index = index,
-                   y = y,
-                   x = x,
-                   output = data.frame(mlblob.output = "No R object Returned, set return_dataframe to TRUE"))
     } else {
-      warning("Returning R object in currently in the works")
+      cat("\nChoosing Models on Test based on Performance and Correlation Thresholds\n")
+      selected_models <- select_models(model_list = all_models,
+                                       test = test,
+                                       eval_metric = eval_metric,
+                                       eval_threshold = eval_threshold,
+                                       y = y,
+                                       correlation_threshold = correlation_threshold)
     }
-    mlout
+  } else {
+    selected_models <- all_models
+  }
+  #=================================================================
+  # predict with the models or selected models.
+  cat("\nSaving Train Predictions with Selected Models\n")
+  train_predictions <- predict_blob(test = train, selected_models)
+  cat("\nSaving Valid Predictions with Selected Models\n")
+  valid_predictions <- predict_blob(test = valid, selected_models)
+  cat("\nSaving Test Predictions with Selected Models\n")
+  test_predictions <- predict_blob(test = test, selected_models)
+  cat("\nPredicting on New Data with Selected Models\n")
+  newdata_predictions <- predict_blob(test = new_data, selected_models)
+
+  # needs work.
+  # make the index dataframe, trivially all 1s for shared holout
+  index = data.frame(beta = "Not finished yet")
+
+  # =================================================
+  if(return_dataframe == FALSE) {
+    # build the output object of new class mlblob
+    mlout <- new("mlblob",
+                  models = selected_models,
+                  labeled_data = list(labeled_data),
+                  train = list(train),
+                  valid = list(valid),
+                  test = list(test),
+                  new_data = list(new_data),
+                  predict_train = train_predictions,
+                  predict_valid = valid_predictions,
+                  predict_test = test_predictions,
+                  predict_newdata = newdata_predictions,
+                  performance = index,
+                  y = y,
+                  x = x,
+                  output = data.frame(mlblob.output = "No R object Returned, set return_dataframe to TRUE"))
+  } else {
+    warning("Returning R object in currently in the works")
+  }
+  mlout
 }
