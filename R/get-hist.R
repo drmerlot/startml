a3eef2114b8ea70197993ab98ef7ab2d90056bbb
andrewsommerlot <andy.sommerlot@gmail.com>
2017-06-21 13:42:12 -0400

    get_hist now uses the stopping metric, rather than rmse every time

--- R/get-hist.R
+++ R/get-hist.R
@@ -2,9 +2,15 @@
 #'
 #' Internal function used in plotting mlblob objects. Retries scoring history from validation given h2o model
 #'
-#' @param x model from mlblob. No Default.
+#' @param model h2o model from mlblob. No Default.
 #' @return Validation history of the model defined by input
 #' @export
-get_hist <- function(x) {
-  x@model$scoring_history$validation_rmse
+get_hist <- function(model){
+  metric <- model@allparameters$stopping_metric
+  if(metric == "RMSLE") {
+    metric <- "RMSE"
+  } else {
+    metric <- metric
+  }
+  model@model$scoring_history[,which(names(model@model$scoring_history) == paste("validation", tolower(metric), sep = "_"))]
 }
