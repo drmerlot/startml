6bf33ebfda0c366ccea394cf18c556a2be98fbd8
andrewsommerlot <andy.sommerlot@gmail.com>
2018-02-12 22:43:15 -0500

    making a demo function that runs startml automatically

---
+++ R/demo.R
@@ -0,0 +1,14 @@
+#' regression_example
+
+## @return Plots example of regression on kaggle housing price data set
+
+# @export
+#demo() {
+#  h20.init()
+#  labeled_data <- h2o.importFile(path = normalizePath("../train.csv"))
+#  newdata <- h2o.importFile(path = normalizePath("../test.csv"))
+
+
+ # startml(labeled_data)
+
+#}
